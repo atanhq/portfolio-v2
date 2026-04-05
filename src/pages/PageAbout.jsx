289f3c3d712c28a85916487224e02e0e8b5597de
atanhq <amtanns@gmail.com>
2026-04-04 20:25:37 -0700

    styled and added content to about page

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -27,48 +27,82 @@ return (
 				<Link to="/"><img src="https://placehold.co/50x50/000000/FFF" /></Link>
 			</div>
 
-			<h2 className="my-story-title">My Story</h2>
+			{/* <h2 className="my-story-title">My Story</h2> */}
 
-			<div className="based-in">
-					<svg xmlns="http://www.w3.org/2000/svg" width="10" height="20" viewBox="0 0 384 512" fa-rotate-90><path d="M32 32C32 14.3 46.3 0 64 0L320 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-29.5 0 10.3 134.1c37.1 21.2 65.8 56.4 78.2 99.7l3.8 13.4c2.8 9.7 .8 20-5.2 28.1S362 352 352 352L32 352c-10 0-19.5-4.7-25.5-12.7s-8-18.4-5.2-28.1L5 297.8c12.4-43.3 41-78.5 78.2-99.7L93.5 64 64 64C46.3 64 32 49.7 32 32zM160 400l64 0 0 112c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-112z"/></svg>
-
+			{/* <div className="based-in">
+					<i class="bi bi-pin-angle-fill"></i>
 					<p className="based-in-tagline">Based in Vancouver, BC</p>
-			</div>
+			</div> */}
 
-			<section className="my-story-extra">
-					<p className="my-story-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec tortor sagittis, porttitor orci nec, malesuada dui. Donec efficitur urna lorem, vel bibendum orci volutpat a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis orci lectus, pulvinar non mattis et, fringilla vitae orci. Morbi tempus arcu et eros commodo, at porttitor lacus ultrices. </p>
+			<section className="about">
 
+				<div className="left">
+					<h1>Hi, I'm Nana!</h1>
+
+					<div className="about-details">
+						<p>I'm a web developer and designer based in <strong>Vancouver, BC</strong>. My passion for designing webpages started with one goal: to make a Myspace page that made people go WOW!</p>
+
+						<p>But with Myspace coming to an end, I found myself looking towards other pursuits. I graduated with a degree in Business Management and Psychology.</p>
+
+						<p>It wasn't until I was well into my corporate career did I realize a future in web development was possible. It was a difficult choice, as is every career change, but when I set foot in BCIT's Front-End Developer Program, I knew this was where I wanted to be.</p>
+
+						<p>So now I'm back here with the same goal: to make you go WOW!</p>
+					</div>
+				</div>
+
+				<div className="right">
 					<div className="toolbox-container">
-							<h2 className="toolbox-title">Toolbox</h2>
-							<div className="tools">
-									tools here
-							</div>
+						<h2 className="toolbox-title">Toolbox</h2>
+
+						<div className="tools">
+							<i class="devicon-react-original"></i>
+							<i class="devicon-javascript-plain"></i>
+							<i class="devicon-php-plain"></i>
+							<i class="devicon-sass-original"></i>
+							<br />
+							<i class="devicon-bootstrap-plain"></i>
+							<i class="devicon-wordpress-plain"></i>
+							<i class="devicon-woocommerce-plain"></i>
+							<i class="devicon-figma-plain"></i>
+							<br />
+							<i class="devicon-photoshop-plain"></i>
+							<i class="devicon-illustrator-plain"></i>
+							<i class="devicon-xd-plain"></i>
+						</div>
 					</div>
 
 					<div className="fun-facts">
 							<Accordion>
 									<Accordion.Item eventKey="0">
-											<Accordion.Header>What are your hobbies?</Accordion.Header>
+											<Accordion.Header>What are some of your hobbies?</Accordion.Header>
 											<Accordion.Body>
-											Lorem ipsum dolor sit amet.
+												<p>Writing and drawing - one of my dreams is to be able to illustrate the stories in my head. I also enjoy learning languages (the spoken kind) and going on runs.</p>
 											</Accordion.Body>
 									</Accordion.Item>
 
 									<Accordion.Item eventKey="1">
-											<Accordion.Header>What's your MBTI?</Accordion.Header>
+											<Accordion.Header>How about your MBTI?</Accordion.Header>
 											<Accordion.Body>
-											Lorem ipsum dolor sit amet.
+												<p>Drumroll please ... INTP! Creative and resourceful, I like to push the envelope of what is possible, analyzing, researching, and plunging myself into the depths of whatever captures my interest.</p>
 											</Accordion.Body>
 									</Accordion.Item>
 
 									<Accordion.Item eventKey="2">
-											<Accordion.Header>What are you learning?</Accordion.Header>
+											<Accordion.Header>Any recs for foodie spots?</Accordion.Header>
 											<Accordion.Body>
-											Lorem ipsum dolor sit amet.
+												<p><strong>Canffle</strong> if you love a good brunch. <strong>East is East</strong> for an amazing dining experience. <strong>La Taqueria</strong> for their nachos. <strong>Paragon</strong> is known for their matcha, but don't sleep on their mango hojicha. And <strong>Just Pizza</strong> - give their adventurous ones a try!</p>
+											</Accordion.Body>
+									</Accordion.Item>
+
+									<Accordion.Item eventKey="3">
+											<Accordion.Header>Give us a fun fact!</Accordion.Header>
+											<Accordion.Body>
+												<p>I've traveled to 12 countries - The United States, Mexico, England, France, Italy, Switzerland, Germany, China, Hong Kong, South Korea, Japan, and Thailand. One day, I'd love to visit Vietnam too.</p>
 											</Accordion.Body>
 									</Accordion.Item>
 							</Accordion>
 					</div>
+				</div>
 			</section>
 			</div>
     </main>
