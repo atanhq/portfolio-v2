2e58f6b6dc0363f93018f20b9ff58d3777b8db93
atanhq <amtanns@gmail.com>
2026-05-11 15:11:47 -0700

    add scroll to content arrow

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { appTitle } from '../globals/appTitle';
+import { HashLink } from "react-router-hash-link";
 import ProjectsAll from '../components/ProjectsAll'
 import Notification from '../components/Notification'
 import Copyright from '../components/Copyright'
 import portrait from '../assets/placeholder-home.png'
+import arrow from '../assets/arrow.svg';
 
 function PageHome({ isOpen }){
 
@@ -22,8 +24,11 @@ function PageHome({ isOpen }){
 						<span>Nana</span>
 						<span>Developer</span>
 						<span>UX/UI Designer</span>
+
+						<HashLink smooth to="/#projects" className="scroll-down"><img src={arrow} alt="arrow pointing down" aria-label="skip to content"/></HashLink>
 					</div>
-					<img src={portrait} alt="portrait" className="home-portrait" />
+
+					<img src={portrait} alt="" className="home-portrait" />
 				</section>
 
 				<section id="projects">
